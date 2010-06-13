c7bbf540cd42f338d333a2019b1e9f59aa48b5dd
Bart Kroon <bart@tarmack.eu>
2010-06-13 22:44:05 +0200

    Ditched a few files that should never have gotten into the repository.

--- test.py
+++
@@ -1,7 +0,0 @@
-def PIcon(icon):#{{{1
-    try:
-        from PyKDE4.kdeui import KIcon
-        return KIcon(icon)
-    except ImportError:
-        from PyQt4.QtGui import QIcon
-        return QIcon('icons/%s.png' % icon)
