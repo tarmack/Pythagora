08b45be7d84666dc97fc278cc696f2b346913ea0
Bart Kroon <bart@tarmack.eu>
2012-04-02 23:13:10 +0200

    Make the configuration dialog a proper class so it can use it's ui.

--- configuration.py
+++ configuration.py
@@ -16,27 +16,27 @@
 # limitations under the License.
 #-------------------------------------------------------------------------------
 from PyQt4.QtCore import QSize, Qt, SIGNAL, QVariant, QSettings
-from PyQt4.QtGui import QIcon, QMessageBox, QFileDialog, QTableWidget, QTableWidgetItem, QLineEdit, QKeySequence, QAction
+from PyQt4.QtGui import QIcon, QMessageBox, QFileDialog, QTableWidget, QTableWidgetItem, QLineEdit, QKeySequence, QAction, QDialog
 import os.path
 import auxilia
 
-from ui import Configuration
+from ui import Configuration as Configuration_Ui
 
 DATA_DIR = ''
 
-class Configuration(Configuration):
+class Configuration(object):
     types = {
                 int: QVariant.toInt,
                 bool: QVariant.toBool,
-                str: QVariant.toString,
+                unicode: QVariant.toString,
                 list: QVariant.toList,
                 QSize: QVariant.toSize,
                 }
     defaults = {
                 'firstTime':            True,
-                'knownHosts':           {'Local': ['localhost','6600','']},
-                'server':               ['Local','localhost','6600',''],
-                'coverPath':            '~/Music/covers',
+                'knownHosts':           {u'Local': [u'localhost', u'6600', u'']},
+                'server':               [u'Local', u'localhost', u'6600', u''],
+                'coverPath':            u'~/Music/covers',
                 'showNotification':     False,
                 'notificationTimeout':  0,
                 'oneLinePlaylist':      False,
@@ -50,7 +50,7 @@ class Configuration(Configuration):
                 'libSplit1':            [3,5],
                 'libSplit2':            [3,5],
                 'playlistSplit':        [5,20],
-                'tabOrder':             ['&Library', 'F&ileSystem', '&PlayLists', '&Shoutcast'],
+                'tabOrder':             [u'&Library', u'F&ileSystem', u'&PlayLists', u'&Shoutcast'],
                 }
 
     def __getattr__(self, attr):
@@ -59,7 +59,7 @@ class Configuration(Configuration):
         except KeyError:
             raise AttributeError("'%s' object has no attribute '%s'" %
                                  (self.__class__.__name__, attr))
-        value = self.__getOption(attr)
+        value = self._getOption(attr)
         setattr(self, attr, value)
         return value
 
@@ -68,7 +68,7 @@ class Configuration(Configuration):
         if attr in self.defaults:
             self.storeOption(attr, value)
 
-    def __getOption(self, option):
+    def _getOption(self, option):
         if option == 'knownHosts':
             settings = QSettings()
             hosts = {}
@@ -107,7 +107,7 @@ class Configuration(Configuration):
                     settings.setValue(name, host)
                 settings.endGroup()
             elif value == None:
-                QSettings().setValue(option, str(value))
+                QSettings().setValue(option, unicode(value))
             else: QSettings().setValue(option, valueType(value))
         except Exception, e:
             print 'error: ', e, '\n', 'error: ', option, value
@@ -119,28 +119,7 @@ class Configuration(Configuration):
     def showConfiguration(self, parent, modal=False):
         '''Display the configuration dialog and activate the changes.'''
         self.parent = parent
-        self.setupUi(self)
-        self.setup.setWindowTitle('Pythagora settings')
-        self.setup.setWindowIcon(QIcon('Pythagora.png'))
-        self.setup.setAttribute(Qt.WA_QuitOnClose, False)
-
-        # Hide dbus options if the module is not present.
-        try:
-            __import__('dbus')
-        except ImportError:
-            self.setup.showNotificationWidget.setVisible(False)
-        self.setup.showNotification.setChecked(self.showNotification)
-        self.setup.notificationTimeout.setValue(self.notificationTimeout)
-
-        self.setup.coverPath.setText(self.coverPath)
-
-        # Setup the serverTable.
-        actionRemove = QAction(auxilia.PIcon("list-remove"), 'Remove', self.setup.serverTable)
-        self.setup.serverTable.addAction(actionRemove)
-        self.setup.serverTable.connect(actionRemove, SIGNAL('triggered()'), self.__removeServer)
-        self.setup.serverTable.setColumnWidth(0, 160)
-        self.setup.serverTable.setColumnWidth(1, 120)
-        self.setup.serverTable.setColumnWidth(2, 50)
+        self.setup = ConfigDialog(parent)
         try:
             for row, server in enumerate(self.knownHosts):
                 self.setup.serverTable.insertRow(row)
@@ -153,7 +132,6 @@ class Configuration(Configuration):
                         self.setup.serverTable.setCellWidget(row, col, password)
                     else:
                         self.setup.serverTable.setItem(row, col, QTableWidgetItem(value))
-            self.__addServer()
             # Select current server.
             if self.server:
                 current = self.setup.serverTable.findItems(self.server[1], Qt.MatchExactly)
@@ -162,19 +140,25 @@ class Configuration(Configuration):
         except Exception, e:
             print 'error: problem showing config', e
 
+        # Hide dbus options if the module is not present.
+        try:
+            __import__('dbus')
+        except ImportError:
+            self.setup.showNotificationWidget.setVisible(False)
+        self.setup.showNotification.setChecked(self.showNotification)
+        self.setup.notificationTimeout.setValue(self.notificationTimeout)
 
-        self.setup.connect(self.setup.serverTable, SIGNAL('cellChanged(int,int)'), self.__cellChanged)
-        self.setup.connect(self.setup.coverDirButton,SIGNAL('clicked()'),self.__selectCoverDir)
-        self.setup.connect(self.setup.buttonBox, SIGNAL('accepted()'), self.__accept)
-        self.setup.serverTable.keyPressEvent = self.__keyPressEvent
+        self.setup.coverPath.setText(self.coverPath)
+
+        self.setup.connect(self.setup.buttonBox, SIGNAL('accepted()'), self._accept)
         if modal:
             self.setup.exec_()
         else: self.setup.show()
 
-    def __accept(self):
+    def _accept(self):
         knownHosts = {}
         for row in xrange(self.setup.serverTable.rowCount()):
-            server = self.__getServer(row)
+            server = self._getServer(row)
             if server:
                 knownHosts.update(server)
         self.knownHosts = knownHosts
@@ -182,7 +166,7 @@ class Configuration(Configuration):
         selection = self.setup.serverTable.selectedItems()
         if selection:
             row = self.setup.serverTable.row(selection[0])
-            for name, host in self.__getServer(row).iteritems():
+            for name, host in self._getServer(row).iteritems():
                 self.server = [name]+host
         else:
             print 'debug: no server selected'
@@ -190,11 +174,11 @@ class Configuration(Configuration):
         self.showNotification = self.setup.showNotification.isChecked()
         self.notificationTimeout = self.setup.notificationTimeout.value()
         self.coverPath = unicode(self.setup.coverPath.text())
-        self.__checkDir("Cover",self.coverPath)
+        _checkDir("Cover", self.coverPath)
         if server[1:] != self.server[1:]:
             self.parent.emit(SIGNAL('reconnect()'))
 
-    def __getServer(self, row):
+    def _getServer(self, row):
         name = self.setup.serverTable.item(row, 0).text()
         host = [self.setup.serverTable.item(row, col).text() for col in [1, 2]]
         host.append(self.setup.serverTable.cellWidget(row, 3).text())
@@ -202,53 +186,76 @@ class Configuration(Configuration):
             return None
         return {unicode(name): [unicode(x) for x in host]}
 
-    def __addServer(self):
-        self.setup.serverTable.blockSignals(True)
-        row = self.setup.serverTable.rowCount()
-        self.setup.serverTable.insertRow(row)
-        for col in range(3):
-            self.setup.serverTable.setItem(row, col, QTableWidgetItem(''))
-        password = QLineEdit()
-        password.setEchoMode(QLineEdit.Password)
-        self.setup.serverTable.setCellWidget(row, 3, password)
-        self.setup.serverTable.blockSignals(False)
 
-    def __keyPressEvent(self, event):
-        if event.matches(QKeySequence.Delete):
-            self.__removeServer()
-        else:
-            QTableWidget.keyPressEvent(self.setup.serverTable, event)
+def _checkDir(name, dir):
+    if dir and not os.path.isdir(os.path.expanduser(unicode(dir))):
+        msgBox = QMessageBox()
+        msgBox.setText("%s Directory does not exist" % name)
+        msgBox.setInformativeText("Do you wish to create it?")
+        msgBox.setStandardButtons(QMessageBox.Yes|QMessageBox.No)
+        ret = msgBox.exec_()
+        if ret == QMessageBox.Yes:
+            os.makedirs(unicode(dir))
 
-    def __removeServer(self):
-        selection = self.setup.serverTable.selectedItems()
-        if selection:
-            row = self.setup.serverTable.row(selection[0])
-            self.__cellChanged(row, 0)
-            self.setup.serverTable.removeRow(row)
 
-    def __cellChanged(self, row, col):
-        if row+1 == self.setup.serverTable.rowCount():
-            self.__addServer()
-        elif col == 1:
-            item = self.setup.serverTable.item(row, 2)
-            if item.text() == '':
-                item.setText('6600')
+class ConfigDialog(QDialog, Configuration_Ui):
+    def __init__(self, parent):
+        QDialog.__init__(self, parent)
+        self.setupUi(self)
+        self.setWindowTitle('Pythagora settings')
+        self.setWindowIcon(QIcon('Pythagora.png'))
+        self.setAttribute(Qt.WA_QuitOnClose, False)
 
-    def __checkDir(self,name,dir):
-        if not os.path.isdir(os.path.expanduser(str(dir))):
-            msgBox = QMessageBox()
-            msgBox.setText("%s Directory does not exist" % name)
-            msgBox.setInformativeText("Do you wish to create it?")
-            msgBox.setStandardButtons(QMessageBox.Yes|QMessageBox.No)
-            ret = msgBox.exec_()
-            if ret == QMessageBox.Yes:
-                os.makedirs(str(dir))
+        # Setup the serverTable.
+        actionRemove = QAction(auxilia.PIcon("list-remove"), 'Remove', self.serverTable)
+        self.serverTable.addAction(actionRemove)
+        self.serverTable.connect(actionRemove, SIGNAL('triggered()'), self._removeServer)
+        self.serverTable.setColumnWidth(0, 160)
+        self.serverTable.setColumnWidth(1, 120)
+        self.serverTable.setColumnWidth(2, 50)
+        self._addServer()
 
-    def __selectCoverDir(self):
-        directory = os.path.expanduser(unicode(self.setup.coverPath.text()))
-        fd = QFileDialog(self.setup, 'Select: Shared Cover Directory', directory)
+        self.connect(self.serverTable, SIGNAL('cellChanged(int, int)'), self._cellChanged)
+        self.connect(self.coverDirButton,SIGNAL('clicked()'),self._selectCoverDir)
+        self.serverTable.keyPressEvent = self._keyPressEvent
+
+    def _selectCoverDir(self):
+        directory = os.path.expanduser(unicode(self.coverPath.text()))
+        fd = QFileDialog(self, 'Select: Shared Cover Directory', directory)
         fd.setFileMode(QFileDialog.DirectoryOnly)
         if fd.exec_() == 1:
-            dir = str(fd.selectedFiles().first())
-            self.setup.coverPath.setText(dir)
+            dir = unicode(fd.selectedFiles().first())
+            self.coverPath.setText(dir)
+
+    def _cellChanged(self, row, col):
+        if row+1 == self.serverTable.rowCount():
+            self._addServer()
+        elif col == 1:
+            item = self.serverTable.item(row, 2)
+            if item and item.text() == '':
+                item.setText('6600')
+
+    def _addServer(self):
+        self.serverTable.blockSignals(True)
+        row = self.serverTable.rowCount()
+        self.serverTable.insertRow(row)
+        for col in range(3):
+            self.serverTable.setItem(row, col, QTableWidgetItem(''))
+        password = QLineEdit()
+        password.setEchoMode(QLineEdit.Password)
+        self.serverTable.setCellWidget(row, 3, password)
+        self.serverTable.blockSignals(False)
+
+    def _removeServer(self):
+        selection = self.serverTable.selectedItems()
+        if selection:
+            row = self.serverTable.row(selection[0])
+            self._cellChanged(row, 0)
+            self.serverTable.removeRow(row)
+
+    def _keyPressEvent(self, event):
+        if event.matches(QKeySequence.Delete):
+            self._removeServer()
+        else:
+            QTableWidget.keyPressEvent(self.serverTable, event)
 
